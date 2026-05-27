405be76d0192f75a706a79a70fcfff86f1714d84
The-Green-Man-12 <masongross144@gmail.com>
2026-05-27 08:52:20 -0400

    Added some backgrounds

---
+++ levels/camera_2d.gd
@@ -0,0 +1,9 @@
+extends Camera2D
+
+func _ready():
+	# Example for an image that occupies from (0,0) to (1920, 1080)
+	limit_left = -575
+	limit_top = -325
+	#limit_right = 5
+	limit_bottom = 325
+	pass
